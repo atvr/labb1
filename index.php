7ccba59d1895af145f3d46493361147b1d169fef
atvr <atvr@users.noreply.github.com>
2013-10-02 17:55:02 +0200

    Update index.php

--- index.php
+++ index.php
@@ -1,5 +1,5 @@
 <?php
-	/* Labb 1 för kursen Webbutveckling med PHP
+	/* Labb 2 för kursen Webbutveckling med PHP
 	   av Per Vrethammar (av222bn) */
 	include_once 'src/controllers/Application.php';
 	$app = new Application();
